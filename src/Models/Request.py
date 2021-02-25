c839bf06adb5f004af97800ca4e8aced43eb3e10
Best40hahaha <xuzinan2@gmail.com>
2021-02-25 17:58:57 +0800

    add feature: request create时加入id提示，方便二轮创建时直接使用

--- src/Models/Request.py
+++ src/Models/Request.py
@@ -111,7 +111,8 @@ class Request:
     def display(self):
         request_vars = vars(self)
         info = ''
-
+        pk_id = MA().query("SELECT max(id) FROM Request")[0][0]
+        info += f'[#E4007F]ID:[/#E4007F] [#00FFFF]{pk_id}[/#00FFFF] \n'
         for item in request_vars:
             if item in ['_request_id', '_owner_first_name', '_owner_last_name', '_mu', '_creation_time',
                         '_last_modified_time',
