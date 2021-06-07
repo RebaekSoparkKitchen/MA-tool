39b01b853fd1b0a94e6172e7749b1988d86b3aea
Best40hahaha <xuzinan2@gmail.com>
2021-06-07 16:10:43 +0800

    feature update: requirement.txt update

--- src/Controller/ExcelExporter.py
+++ src/Controller/ExcelExporter.py
@@ -14,12 +14,27 @@ class ExcelExporter(object):
             f.write(rows.export('xlsx'))
 
 if __name__ == '__main__':
-    e = ExcelExporter("select * from request left outer join BasicPerformance using (smc_campaign_id) where "
-                      "(team == 'HK Marketing' or team == 'CX Demand Generation Hong Kong') and date(blast_date) >= "
-                      "date('2021-01-01') "
-                      "--case-insensitive ",
-                      'C:/Users/C5293427/Desktop/EDM/analytics_data/HK_2021_1Q.xlsx')
+
+    # hk
+    # e = ExcelExporter("select * from request left outer join BasicPerformance using (smc_campaign_id) where "
+    #                   "(team == 'HK Marketing' or team == 'CX Demand Generation Hong Kong') and date(blast_date) >= "
+    #                   "date('2021-01-01') "
+    #                   "--case-insensitive ",
+    #                   'C:/Users/C5293427/Desktop/EDM/analytics_data/HK_2021_1Q.xlsx')
+
     # e = ExcelExporter("select id, blast_date from request where "
     #                   "date(blast_date) between date('2020-01-01') and date('2020-12-31') ",
     #                   'C:/Users/C5293427/Desktop/EDM/analytics_data/', 'rise_with_sap.xlsx')
+
+    e = ExcelExporter("select * from request left outer join BasicPerformance using (smc_campaign_id) where "
+                      "comments like '%sapphire%' and date(blast_date) >= "
+                      "date('2021-01-01') "
+                      "--case-insensitive ",
+                      'C:/Users/C5293427/Desktop/EDM/analytics_data/CN_2021_sapphire.xlsx')
+
+    # e = ExcelExporter("select * from request left outer join BasicPerformance using (smc_campaign_id) where "
+    #                   "date(blast_date) >= "
+    #                   "date('2020-01-01') "
+    #                   "--case-insensitive ",
+    #                   'C:/Users/C5293427/Desktop/EDM/analytics_data/bounce_rate.xlsx')
     e.export()
